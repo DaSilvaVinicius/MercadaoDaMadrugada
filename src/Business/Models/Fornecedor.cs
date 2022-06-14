160091a5e5b01aac96f895a794d9ab52548d0363
ViniOPrincesoGuerreiro <vinicius.da.silva@bol.com.br>
2022-06-14 13:29:44 -0300

    Adicionando models na camada de negocio

---
+++ src/Business/Models/Fornecedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public int Telefone { get; set; }
+        public string Documento { get; set; }
+        public TipoDocumento TipoDocumento { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Endereco Endereco { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
---
+++ src/Business/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Models
+{
+    public class Produto : Entity
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public decimal Valor { get; set; }
+        public bool EstoqueDisponivel { get; set; }
+        public Fornecedor Fornecedor { get; set; }
+        public Guid FornecedorId { get; set; }
+    }
+}
